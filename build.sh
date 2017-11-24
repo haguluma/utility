9df0d495d4ec1e946424c68fc21be2416284da99
kotoda <kotoda@ISDL-Public-iMac.local>
2017-11-24 17:50:41 +0900

    build.shを修正

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-mkdir -p Cglog/memory/$HOSTNAME/memory_log
-mkdir -p Cglog/cpu/$HOSTNAME/cpu_log
+mkdir -p Cglog/memory/$HOSTNAME
+touch Cglog/memory/$HOSTNAME/memory_log
+mkdir -p Cglog/cpu/$HOSTNAME
+touch Cglog/cpu/$HOSTNAME/cpu_log
 sudo chmod 777 -R Cglog/memory/$HOSTNAME/memory_log
 sudo chmod 777 -R Cglog/cpu/$HOSTNAME/cpu_log
