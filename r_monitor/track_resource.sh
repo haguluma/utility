865821cedbc1b799fea957e67a3a875a41c101f3
haguluma0 <haguluma0@gmail.com>
2017-11-27 20:14:50 +0900

    track_resourceãfix track_resource error

--- r_monitor/track_resource.sh
+++ r_monitor/track_resource.sh
@@ -2,11 +2,11 @@
 : > /home/general/utility/Cglog/cpu/$HOSTNAME/cpu_log
 
 while true; do
-#   echo 0 | sudo tee /sys/fs/cgroup/memory/trackmemory/memory.max_usage_in_bytes >/dev/null
-   echo 0 | sudo tee /sys/fs/cgroup/cpu/cpu_ctl/cpuacct.usage >/dev/null
+   echo 0 > /sys/fs/cgroup/memory/memory_ctl/memory.max_usage_in_bytes 
+   echo 0 > /sys/fs/cgroup/cpu/cpu_ctl/cpuacct.usage
    sleep 1
-   echo "$(date +%M.%S.%N | sed -e 's/\./ /g' | gawk '{print $1*60000000000+$2*1000000000+$3}'):$(cat /sys/fs/cgroup/memory/memory_ctl/memory.usage_in_bytes)" | tee -a /home/general/utility/Cglog/memory/$HOSTNAME/memory_log > /dev/null
-   echo "$(date +%M.%S.%N | sed -e 's/\./ /g' | gawk '{print $1*60000000000+$2*1000000000+$3}'):$(cat /sys/fs/cgroup/cpu/cpu_ctl/cpuacct.usage)" | tee -a /home/general/utility/Cglog/cpu/$HOSTNAME/cpu_log > /dev/null
+   echo "$(date +%M.%S.%N | sed -e 's/\./ /g' | gawk '{print $1*60000000000+$2*1000000000+$3}'):$(cat /sys/fs/cgroup/memory/memory_ctl/memory.usage_in_bytes)" | tee -a /home/general/utility/Cglog/memory/$HOSTNAME/memory_log #> /dev/null
+   echo "$(date +%M.%S.%N | sed -e 's/\./ /g' | gawk '{print $1*60000000000+$2*1000000000+$3}'):$(cat /sys/fs/cgroup/cpu/cpu_ctl/cpuacct.usage)" | tee -a /home/general/utility/Cglog/cpu/$HOSTNAME/cpu_log #> /dev/null
 #   echo "----------------"
 done 
 
